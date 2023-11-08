a1571ec89c7a2e4725caa10d5aed176214cebce8
Rubens Diniz <rubensadiniz@gmail.com>
2023-11-08 19:08:36 -0300

    Fix minor theme problem and add basic mobile padding

--- src/library/CustomLink/index.ts
+++ src/library/CustomLink/index.ts
@@ -10,10 +10,10 @@ export const CustomLink = styled(Link)`
 
   background-position: bottom left;
   background-size: 0 2px;
-  background-image: linear-gradient(var(--text), var(--text));
+  background-image: linear-gradient(var(--text), var(--text));  
   background-repeat: no-repeat;
 
-  &:hover {
+  &:hover, &:focus {
     background-size: 100% 2px;
     font-weight: 600;
   }
